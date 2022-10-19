5c8541a5ce8c3c16b8ecaaa24a4eca9edddb4aa0
valkim55 <vesper5521@gmail.com>
2022-10-19 14:53:01 -0400

    back-end complete

---
+++ routes/apiRoutes/voteRoutes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+// ===== POST route for voting ====
+router.post('/vote', ({body}, res) => {
+    const errors = inputCheck(body, 'voter_id', 'candidate_id');
+    if(errors) {
+        return res.status(400).json({error: errors});
+    }
+
+    const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?, ?)`;
+    const params = [body.voter_id, body.candidate_id];
+
+    db.query(sql, params, (err, result) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        } 
+        res.json({
+            message: 'success', 
+            data: body, 
+            changes: result.affectedRows
+        });
+    });
+});
+
+// ===== GET total votes for each candidate in descending order =====
+router.get('/votes', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count FROM votes
+                LEFT JOIN candidates ON votes.candidate_id = candidates.id LEFT JOIN parties ON candidates.party_id = parties.id
+                GROUP BY(candidate_id) ORDER BY count DESC`;
+
+    db.query(sql, (err, rows) => {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({message: 'success', data: rows});
+    });
+});
+
+
+
+module.exports = router;
